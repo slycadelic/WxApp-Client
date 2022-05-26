ffa323c5b45c74bea94dbdd54f0385b927031041
Shivam Patel <38561152+slycadelic@users.noreply.github.com>
2022-05-26 15:57:52 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://ec2-13-233-71-124.ap-south-1.compute.amazonaws.com:80';
+const BASE_URL = 'https://ec2-13-233-71-124.ap-south-1.compute.amazonaws.com:80';
 
 export default axios.create({
     baseURL: BASE_URL
